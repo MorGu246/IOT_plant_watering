a20497134561be9fe177e0f2d1d14b71741df65d
Mor Guetta <morgueta36@gmail.com>
2025-11-25 19:07:02 +0200

    commit2_with_the_server_and_env

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const routes = require("./routes")
+
+const port = process.env.pory || 3671;
+const app = express();
+
+app.use(express.static(path.join(__dirname,"../public"))); // סטטיק זה שנוכל לגשת לתיקייה או קובץ מכל מקום בקוד מבלי לנתב אליו
+app.use(express.json());
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use("/",routes);
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`The server is running on port${port}....`);
+});
